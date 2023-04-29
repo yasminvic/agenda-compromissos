1f9866221d1b6f6759e6b7498b96ba776dc3bf7c
Yasmin <yasminalvesdesouza27@gmail.com>
2023-04-29 14:23:03 -0300

    Feat: Ajustando Url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production : false,
-  apiUrlPost : "http://localhost:5000/incluir/",
+  apiUrlPost : "http://localhost:5000/incluir",
   apiUrlGet: "http://localhost:5000/getAll/"
 }
